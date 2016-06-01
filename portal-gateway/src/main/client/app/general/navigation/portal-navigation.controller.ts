dd9a05347affc5a0d96d20c1b0f2fc21f302923d
Marek Matczak <marek.matczak@capgemini.com>
2016-06-01 09:00:04 +0200

    Navigation taken from Bootstrap

--- portal-gateway/src/main/client/app/general/navigation/portal-navigation.controller.ts
+++ portal-gateway/src/main/client/app/general/navigation/portal-navigation.controller.ts
@@ -2,22 +2,31 @@ import { Router, IApplication } from '../../router/router.service';
 
 export class PortalNavigationCtrl {
   apps:NavigationElement[];
+  navCollapsed:boolean = true;
 
   /* @ngInject */
-  constructor(router:Router) {
+  constructor(private router:Router) {
     this.apps = [];
-    this.apps.push(new NavigationElement('#/portal/welcome', 'Home'));
+    this.apps.push(new NavigationElement('home', '#/portal/home', 'Home'));
     let applications:IApplication[] = router.getApplicationsUserIsAuthorizedFor();
     applications.forEach((app:IApplication) => {
       let href = '#/' + app.id + '/';
       let label = app.label || app.id;
-      this.apps.push(new NavigationElement(href, label));
+      this.apps.push(new NavigationElement(app.id, href, label));
     });
+  }
 
+  toggleNavigation():void {
+    this.navCollapsed = !this.navCollapsed;
+  }
+
+  isAppCurrentOne(app:NavigationElement):boolean {
+    let appIdOrStateName = this.router.getIdOrStateNameOfCurrentApp();
+    return app.appId === appIdOrStateName;
   }
 }
 
 class NavigationElement {
-  constructor(public href:string, public label:string) {
+  constructor(public appId:string, public href:string, public label:string) {
   }
 }
