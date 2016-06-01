ab267a024ae4c989ed2f17871317b2040de0389a
Marek Matczak <marek.matczak@capgemini.com>
2016-06-01 08:58:19 +0200

    Refactoring

--- portal-gateway/src/test/java/com/capgemini/devon/PortalGatewayApplicationTests.java
+++ portal-gateway/src/test/java/com/capgemini/devon/PortalGatewayApplicationTests.java
@@ -3,6 +3,9 @@ package com.capgemini.devon;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.capgemini.devon.portalgateway.PortalGatewayApplication;
+
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
